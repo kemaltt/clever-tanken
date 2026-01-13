151241f8d3adc57779c39f28c7bd98ab6bceaa24
kemaltt <ttnckemal@gmail.com>
2026-01-14 00:18:12 +0100

    last commit

--- mobile/constants/Config.ts
+++ mobile/constants/Config.ts
@@ -2,7 +2,7 @@ const API_IP = "192.168.178.29";
 const API_PORT = "4000";
 
 export default {
-  API_BASE_URL: `http://${API_IP}:${API_PORT}/api`,
+  API_BASE_URL: `https://gunstiger-tanken.vercel.app/api`,
   API_IP,
   API_PORT,
 };
